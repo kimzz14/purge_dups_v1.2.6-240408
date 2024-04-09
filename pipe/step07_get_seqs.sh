0ae3a1c518523ff699a51a39366d5d7ac3ccbc4c
kimzz14 <kimzz1004@gmail.com>
2024-04-09 13:15:08 +0900

    log

--- pipe/step07_get_seqs.sh
+++ pipe/step07_get_seqs.sh
@@ -1,5 +1,5 @@
 echo '[step07_get_seqs] start : '`date +%y-%m-%d` `date +%T` 
 
-get_seqs -e dups.bed ref.fa
+get_seqs -e dups.bed ref.fa &> purged.fa.log
 
 echo '[step07_get_seqs] done  : '`date +%y-%m-%d` `date +%T`
