c5e79e6f40eadb3cae86e1000160d05b9f00fc77
kimzz14 <kimzz1004@gmail.com>
2024-04-09 13:05:28 +0900

    log

--- run01_purge_dups.sh
+++ run01_purge_dups.sh
@@ -1,7 +1,7 @@
-sh pipe/step01_minimap2-pb.sh
-sh pipe/step02_pbcstat.sh
-sh pipe/step03_calcuts.sh
-sh pipe/step04_split_fa.sh
-sh pipe/step05_minimap2-self.sh
-sh pipe/step06_purge_dups.sh
-sh pipe/step07_get_seqs.sh
+sh pipe/step01_minimap2-pb.sh   >> purge_dups.log
+sh pipe/step02_pbcstat.sh       >> purge_dups.log
+sh pipe/step03_calcuts.sh       >> purge_dups.log
+sh pipe/step04_split_fa.sh      >> purge_dups.log
+sh pipe/step05_minimap2-self.sh >> purge_dups.log
+sh pipe/step06_purge_dups.sh    >> purge_dups.log
+sh pipe/step07_get_seqs.sh      >> purge_dups.log
