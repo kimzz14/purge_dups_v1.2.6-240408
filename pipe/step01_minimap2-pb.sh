4d5a0c06605e9c19a085fb8e98189af11277f3f3
kimzz14 <kimzz1004@gmail.com>
2024-04-09 18:14:43 +0900

    thread

--- pipe/step01_minimap2-pb.sh
+++ pipe/step01_minimap2-pb.sh
@@ -2,6 +2,6 @@ echo '[step01_minimap2] start : '`date +%y-%m-%d` `date +%T`
 pb_list=`ls *.fastq.gz`
 for pb in $pb_list
 do
-       minimap2 -x asm20 -I 20G ref.fa $pb 2> $pb.paf.log | gzip -c - 1> $pb.paf.gz 2> $pb.paf.gz.log
+       minimap2 -x asm20 -I 20G -t 128 ref.fa $pb 2> $pb.paf.log | gzip -c - 1> $pb.paf.gz 2> $pb.paf.gz.log
 done
 echo '[step01_minimap2] done  : '`date +%y-%m-%d` `date +%T`
--- pipe/step05_minimap2-self.sh
+++ pipe/step05_minimap2-self.sh
@@ -1,5 +1,5 @@
 echo '[step05_minimap2-self] start : '`date +%y-%m-%d` `date +%T` 
 
-minimap2 -x asm5 -DP ref.split.fa ref.split.fa 2> ref.split.self.paf.log | gzip -c - > ref.split.self.paf.gz
+minimap2 -x asm5 -DP -t 128 ref.split.fa ref.split.fa 2> ref.split.self.paf.log | gzip -c - > ref.split.self.paf.gz
 
 echo '[step05_minimap2-self] done  : '`date +%y-%m-%d` `date +%T`
